e08528cad3c5fa14ce832f8da4c2ac123d4111d0
Nicolas Brugneaux <nicolas.brugneaux@gmail.com>
2013-03-08 15:37:32 +0100

    css
    added some style for home & contact
    TODO : contact form still not working / commentary system

--- blog/forms.py
+++ blog/forms.py
@@ -1,7 +1,14 @@
 from django import forms
 
 class ContactForm(forms.Form):
+
 	sujet = forms.CharField(max_length=100,required=True)
+	sujet.widget.attrs.update({'class' : 'inputs'})
+
 	message = forms.CharField(widget=forms.Textarea,required=True)
+	message.widget.attrs.update({'class' : 'inputs'})
+
 	envoyeur = forms.EmailField(label="Votre adresse mail",required=True)
+	envoyeur.widget.attrs.update({'class' : 'inputs'})
+
 	renvoi = forms.BooleanField(help_text="Cochez si vous souhaitez obtenir une copie du mail envoyé.", required=False)
