9cf4a64c528add88c6d4379180068268088e395c
Nicolas Brugneaux <nicolas.brugneaux@gmail.com>
2013-03-08 18:38:50 +0100

    post reversed
    display posts ordered by date (last post first)

--- blog/views.py
+++ blog/views.py
@@ -44,5 +44,5 @@ def contact(request):
 
 
 def home(request):
-	articles = Article.objects.all()
+	articles = Article.objects.order_by('date').reverse()
 	return render(request, 'blog/home.html', {'derniers_articles':articles, 'last':articles[0]})
